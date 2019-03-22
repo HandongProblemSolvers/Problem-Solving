3ea4b2173c41188eb6080df3c052fe08d80f2429
Odsn <21500121@handong.edu>
2019-03-23 00:58:09 +0900

    Try to solve BOJ5626

---
+++ problems/BOJ 5626/김용주.cpp
@@ -0,0 +1,42 @@
+//BOJ5626 제단
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+#define MAX_N 10001
+#define MOD 1000000007
+typedef long long ll;
+
+int N, arr[MAX_N], h;
+ll dp[MAX_N], prv[MAX_N];
+
+int main() {
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++)
+		scanf("%d", &arr[i]);
+	//처음 input이 1이상일 수도 있음. 그럴 경우는 없으니 0.
+	prv[0] = arr[0] <= 0 ? 1 : 0;
+	//prv가 이전 열, dp가 현재 열.
+    for (int i = 1; i < N; i++) {
+		memset(dp, 0, sizeof(dp));
+		if (arr[i] == -1) {
+			for (int j = 0; j < N / 2; j++) {
+				if (j == 0) dp[j] = (prv[0] + prv[1]) % MOD;
+				else dp[j] = (prv[j - 1] + prv[j] + prv[j + 1]) % MOD;
+			}
+		}
+
+		else if (arr[i] == 0)
+			dp[0] = (prv[0] + prv[1]) % MOD;
+
+		else {
+			h = arr[i];
+			dp[h] = (prv[h - 1] + prv[h] + prv[h + 1]) % MOD;
+		}
+        //넘어가기 전에 바꿔주면서 prv가 update
+		swap(prv, dp);
+	}
+    //prv[0] = dp[0] + dp[1]. 여기서 dp는 바로 앞열의 경우의 수가 저장되어 있음.
+	printf("%lld", prv[0]);
+	return 0;
+}
