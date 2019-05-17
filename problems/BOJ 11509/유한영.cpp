d94d6e68cebc199886617a2be4a2a9014143a489
hanyoung0411 <43256337+hanyoung0411@users.noreply.github.com>
2019-05-17 16:24:19 +0900

    Update 유한영.cpp

--- problems/BOJ 11509/유한영.cpp
+++ problems/BOJ 11509/유한영.cpp
@@ -1,20 +1,14 @@
-
 #include <iostream>
 using namespace std;
 int main() {
-    int N,tmp,result = 0;
-    int arrow[1000001];
+    ios::sync_with_stdio(false);
+    int N,tmp,result = 0,arrow[1000001];
     cin>>N;
     for(int i=0;i<N;i++){
         cin>>tmp;
-        if(arrow[tmp]>0){
-            arrow[tmp]--;
-            arrow[tmp-1]++;
-        }
-        else if(arrow[tmp]==0){
-            arrow[tmp-1]++;
-            result ++;
-        }
+        if(arrow[tmp])arrow[tmp]--;
+        else result ++;
+        arrow[tmp-1]++;
     }
     cout<<result;
     return 0;
