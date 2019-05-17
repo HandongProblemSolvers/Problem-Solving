82ae809a2a0a8b2c4c6f8dd051cebd59c3b351e8
gichul <37562319+gichul@users.noreply.github.com>
2019-05-18 03:27:47 +0900

    Update 김기철.java

--- problems/BOJ 11509/김기철.java
+++ problems/BOJ 11509/김기철.java
@@ -1,15 +1,11 @@
 import java.util.*;
-
-public class Main{
+public class P11509{
 	public static void main(String[] args){
 		Scanner sc=new Scanner(System.in);
-		int N;
-		int[] H;
-		int[] M;
+		int N=sc.nextInt();
+		int[] H=new int[N+1];
+		int[] M=new int[N+1];
 		int ans=0;
-		N=sc.nextInt();
-		H=new int[N+1];
-		M=new int[N+1];
 		for(int i=1; i<=N;i++){
 			H[i]=sc.nextInt();
 			if(M[H[i]]==0)ans++;
