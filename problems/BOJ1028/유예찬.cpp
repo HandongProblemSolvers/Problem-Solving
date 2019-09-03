bce8cd959bd8b57ee9ccd0d12261807619da9b81
fbdp1202 <rbynddd@naver.com>
2019-09-03 19:24:13 +0900

    Solve BOJ 1028 yechan

--- problems/BOJ1028/유예찬.cpp
+++ problems/BOJ1028/유예찬.cpp
@@ -4,36 +4,29 @@
 using namespace std;
 
 const int MAX_R=752;
-const int dir[2][2] = {{-1, 1}, {-1, -1}};
 
-char str[MAX_R];
-int R, C, mine[MAX_R][MAX_R];
-int dp[2][MAX_R][MAX_R];
+int R, C, dp[2][MAX_R][MAX_R];
+char mine[MAX_R][MAX_R];
 
 int main()
 {
 	scanf("%d%d", &R, &C);
-	for (int i=1; i<=R; i++) {
-		scanf("%s", str+1);
-		for (int j=1; j<=C; j++) {
-			mine[i][j] = str[j]-'0';
-		}
-	}
+	for (int i=1; i<=R; i++)
+		scanf("%s", mine[i]+1);
 
-	for (int d=0; d<2; d++)
-		for (int i=1; i<=R; i++)
-			for (int j=1; j<=C; j++)
-				if (mine[i][j])
-					dp[d][i][j] = dp[d][i+dir[d][0]][j+dir[d][1]] + 1;
+	for (int i=R; i>=1; i--)
+		for (int j=C; j>=1; j--)
+			if (mine[i][j] == '1') {
+				dp[0][i][j] = dp[0][i+1][j+1] + 1; // (left up arrow)
+				dp[1][i][j] = dp[1][i+1][j-1] + 1; // (right up arrow)
+			}
 
-	// i + 1 = size of dia
-	// min(dp(0, x+i, y), dp(0, x+i*2, y+i), dp(1, x+i*2, y+i), dp(1, x+i, y+i*2)) >= i, dia size is bigger than 'i'
 	int ret=0;
-	for (int i=0; i*2+1<=min(R,C); i++)
-		for (int x=1; x+2*i<=R; x++)
-			for (int y=1; y+2*i<=C; y++)
-				if (min(dp[0][i+x][y], min(dp[0][i*2+x][i+y], min(dp[1][i*2+x][i+y], dp[1][i+x][i*2+y]))) >= i+1)
-					ret = max(ret, i+1);
+	for (int i=1; i<=R; i++)
+		for (int j=1; j<=C; j++)
+			for (int k=min(dp[0][i][j], dp[1][i][j]); k > ret; k--)
+				if (min(dp[0][i+k-1][j-k+1], dp[1][i+k-1][j+k-1]) >= k)
+					ret = k;
 
 	printf("%d\n", ret);
 	return 0;
